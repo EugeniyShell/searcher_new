76fb8b11c2d18ed6c7d7aaa609e3e96529bb4f6b
Jorian <jorian@mail.ru>
2023-07-02 00:01:17 +0300

    less fixes

--- main/base_renew.py
+++ main/base_renew.py
@@ -9,6 +9,14 @@ from main.defs import GRLS_ADDRESS, USERAGENT, SOURCEPATH
 
 
 def base_renew():
+    get_grls()
+    for file in SOURCEPATH.glob('*.zip'):
+        process_zips(file)
+    for file in SOURCEPATH.glob('*.xls'):
+        process_xls(file)
+
+
+def get_grls():
     url = GRLS_ADDRESS + 'GRLS.aspx'
     headers = {
         'User-agent': USERAGENT,
@@ -19,19 +27,21 @@ def base_renew():
         url = root.xpath('//div[@id="ctl00_plate_tdzip"]/button/@onclick')[0]
         url = GRLS_ADDRESS + url.split("'")[1]
         wget.download(url, str(SOURCEPATH))
-        for file in SOURCEPATH.glob('*.zip'):
-            unarchive_grls(file)
     except Exception as err:
         print(err)
 
 
-def unarchive_grls(file):
-    with ZipFile(file, 'r') as zip:
-        for name in zip.namelist():
+def process_zips(file):
+    with ZipFile(file, 'r') as zf:
+        for name in zf.namelist():
             unicode_name = name.encode('cp437').decode('cp866')
-            with zip.open(name) as z:
+            with zf.open(name) as z:
                 content = z.read()
                 fullpath = SOURCEPATH / unicode_name
                 with open(fullpath, 'wb') as f:
                     f.write(content)
     os.remove(file)
+
+
+def process_xls(file):
+    pass
